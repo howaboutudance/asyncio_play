9bac6b4419e4eeb73cbb5d5cfab757549b1b192d
Michael Penhallegon <michael.penhallegon@crowncastle.com>
2024-04-05 16:00:51 -0700

    Write Configurable Log Func Handler, set up testing in vscode Fixes #3

---
+++ asyncio_semaphore_play/log.py
@@ -0,0 +1,61 @@
+"""Logging utilities for package."""
+import logging
+
+from pathlib import Path
+
+
+class FuncNameExceptionFilter(logging.Filter):
+    """A logging filter that filters by function name and log level."""
+    def __init__(self, func_name: str, level: int = logging.ERROR):
+        """Initialize the filter.
+        
+        :param func_name: The name of the function to filter by.
+        :param level: The logging level to filter by.
+        """
+        # set the function name and log level to private attributes
+        self._func_name = func_name
+        self._log_level = level
+    
+    def filter(self, record):
+        """Filter the log record based on function name and log level.
+        
+        :param record: The log record to filter.
+        :return: True if the record should be logged, False otherwise.
+        """
+        # Check if the record has the correct function name and log level
+        if record.funcName == self._func_name and record.levelno >= self._log_level:
+            return True
+        else:
+            return False
+
+
+def setup_func_exception_log_handler(
+        func_name: str, 
+        file_path: str | Path = None, 
+        log_level: int = logging.ERROR, 
+        log_format: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    ):
+    """Setup a logging handler that filters by function name and log level.
+
+    Can setup either a file handler or a stream handler based on file_path. can
+    configured to filter by log level and function name and has configurable log formatter.
+    
+    :param func_name: The name of the function to filter by.
+    :param file_path: The path to the file to write logs to. If None, logs will be written to stderr.
+    :param log_level: The logging level to filter by.
+    :param log_format: The format of the log messages.
+    :return: A logging handler.
+    """
+    # Setup the handler, if file_path is None, use a stream handler, otherwise use a file handler
+    if file_path is None:
+        handler = logging.StreamHandler()
+    else:
+        handler = logging.FileHandler(file_path, mode="w")
+    
+    # Set the log level, add the filter, and set the formatter
+    handler.setLevel(log_level)
+    handler.addFilter(FuncNameExceptionFilter(func_name, log_level))
+    handler.setFormatter(logging.Formatter(log_format))
+
+    # Return the handler
+    return handler
---
+++ test/test_log.py
@@ -0,0 +1,55 @@
+import logging
+from unittest.mock import patch, Mock
+
+import pytest
+
+from asyncio_semaphore_play import log
+
+@pytest.mark.parametrize(
+        ["m_file_path", "expected_class"],
+        [
+            [None, logging.StreamHandler],
+            ["test.log", logging.FileHandler]
+        ]
+)
+def test_setup_func_exception_log_handler_file_handler(m_file_path, expected_class):
+    """Test that the setup_func_exception_log_handler function returns the correct handler."""
+    m_log_level = logging.INFO
+    m_func_name = "test_func"
+    m_format_str = "%(asctime)s - %(name)s: %(funcName)s - %(levelname)s - %(message)s"
+
+    with patch("asyncio_semaphore_play.log.FuncNameExceptionFilter", autospec=True) as m_func_name_filter:
+        # Setup handler and check that it is the correct class
+        handler = log.setup_func_exception_log_handler(m_func_name, m_file_path, m_log_level, m_format_str)
+        assert isinstance(handler, expected_class)
+
+        # Check that handler has correct level and format
+        assert handler.level == m_log_level
+        assert handler.formatter._fmt == m_format_str
+
+        # Make sure the filter was added to the handler
+        m_func_name_filter.assert_called_once_with(m_func_name, m_log_level)
+
+def test_func_name_exception_filter():
+    """Test that the FuncNameExceptionFilter filter works as expected."""
+    m_func_name = "test_func"
+    m_log_level = logging.INFO
+    m_record = Mock(funcName=m_func_name, levelno=m_log_level)
+
+    # Test that the filter returns True when the record has the correct funcName and level
+    filter = log.FuncNameExceptionFilter(m_func_name, m_log_level)
+    assert filter.filter(m_record) == True
+
+    # Test that the filter returns False when the record has the correct funcName but the wrong level
+    m_record.levelno = logging.DEBUG
+    assert filter.filter(m_record) == False
+
+    # Test that the filter returns False when the record has the wrong funcName but the correct level
+    m_record.funcName = "wrong_func"
+    m_record.levelno = m_log_level
+    assert filter.filter(m_record) == False
+
+    # Test that the filter returns False when the record has the wrong funcName and the wrong level
+    m_record.funcName = "wrong_func"
+    m_record.levelno = logging.DEBUG
+    assert filter.filter(m_record) == False
