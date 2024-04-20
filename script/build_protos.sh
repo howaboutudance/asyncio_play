eee96fa4bfb0c398ff0049da1e2ff384b586b033
Michael Penhallegon <mike@hematite.tech>
2024-04-20 18:09:42 +0000

    add all the infrastructure required to build protobufs

---
+++ script/build_protos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# change working directory to protos
+cd ./protos
+
+# Directory paths
+proto_dir="./activitypub_service"
+output_dir="../activitypub_service"
+
+# Check if output directory exists, create if needed
+if [ ! -d "$output_dir" ]; then
+    mkdir -p "$output_dir"
+fi
+
+# Loop through proto files in the directory
+for file in "$proto_dir"/*.proto; do
+    # Generate Python modules using grpcio
+    python -m grpc_tools.protoc -I"$proto_dir" --python_out="$output_dir" --grpc_python_out="$output_dir" "$file"
+done
+
+# change working directory back to root
+cd ..
