c7fb898e70fa8b1d0e22f84e9793f64dbb52cb42
Michael Penhallegon <mike@hematite.tech>
2024-04-28 01:11:57 +0000

    linting + formatting

--- src/asyncio_play/event_parsing.py
+++ src/asyncio_play/event_parsing.py
@@ -1,4 +1,5 @@
 """Event parsing module."""
+
 import json
 
 from asyncio_play.schema import Event
@@ -7,11 +8,13 @@ from asyncio_play.schema import Event
 def json_to_event_obj(event_json: str) -> Event:
     """Parse a json string into an Event object."""
     event_dict = json.loads(event_json)
-    if (event_dict["type"] != "Event") or event_dict["@context"]!= "https://www.w3.org/ns/activitystreams":
+    if (event_dict["type"] != "Event") or event_dict[
+        "@context"
+    ] != "https://www.w3.org/ns/activitystreams":
         raise ValueError("The json is not an Event object.")
 
     return Event(
         name=event_dict["name"],
         start_time=event_dict["startTime"],
-        end_time=event_dict["endTime"]
+        end_time=event_dict["endTime"],
     )
--- src/asyncio_play/log.py
+++ src/asyncio_play/log.py
@@ -1,23 +1,25 @@
 """Logging utilities for package."""
+
 import logging
 from pathlib import Path
 
 
 class FuncNameExceptionFilter(logging.Filter):
     """A logging filter that filters by function name and log level."""
+
     def __init__(self, func_name: str, level: int = logging.ERROR):
         """Initialize the filter.
-        
+
         :param func_name: The name of the function to filter by.
         :param level: The logging level to filter by.
         """
         # set the function name and log level to private attributes
         self._func_name = func_name
         self._log_level = level
-    
+
     def filter(self, record):
         """Filter the log record based on function name and log level.
-        
+
         :param record: The log record to filter.
         :return: True if the record should be logged, False otherwise.
         """
@@ -29,16 +31,16 @@ class FuncNameExceptionFilter(logging.Filter):
 
 
 def setup_func_exception_log_handler(
-        func_name: str, 
-        file_path: str | Path = None, 
-        log_level: int = logging.ERROR, 
-        log_format: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    ):
+    func_name: str,
+    file_path: str | Path = None,
+    log_level: int = logging.ERROR,
+    log_format: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+):
     """Setup a logging handler that filters by function name and log level.
 
     Can setup either a file handler or a stream handler based on file_path. can
     configured to filter by log level and function name and has configurable log formatter.
-    
+
     :param func_name: The name of the function to filter by.
     :param file_path: The path to the file to write logs to. If None, logs will be written to stderr.
     :param log_level: The logging level to filter by.
@@ -50,7 +52,7 @@ def setup_func_exception_log_handler(
         handler = logging.StreamHandler()
     else:
         handler = logging.FileHandler(file_path, mode="w")
-    
+
     # Set the log level, add the filter, and set the formatter
     handler.setLevel(log_level)
     handler.addFilter(FuncNameExceptionFilter(func_name, log_level))
